172339c79acaf01caa1e84fe11ca38a54b90db41
EffandiAhmadTurk <102308867+EffandiAhmadTurk@users.noreply.github.com>
2022-03-24 21:25:55 +0500

    Add files via upload

---
+++ Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+    return <div>
+    <>
+   
+ <div>
+ <p className="text-5xl text-blue-300">hello world</p>
+ </div>
+  
+    </>
+     
+    </div>
+    
+  }
+  
+  export default Navbar
